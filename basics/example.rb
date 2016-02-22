282ab67d612d457a9accca43ac686c24cfcdd15c
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-22 12:05:00 +0100

    updating constructor [#5]

--- basics/example.rb
+++ basics/example.rb
@@ -2,12 +2,12 @@ class Article
   attr_reader :title, :body, :author, :created_at
   attr_accessor :likes, :dislikes
 
-  def initialize(title, body, author, created_at, likes, dislikes)
+  def initialize(title, body, author = nil)
     @title = title
     @body = body
     @author = author
-    @created_at = created_at
-    @likes = likes
-    @dislikes = dislikes
+    @created_at = Time.now
+    @likes = 0
+    @dislikes = 0
   end
 end
