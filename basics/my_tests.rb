e3ebce8d2b25b8bfcbe48564ae44749c43e2847a
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-23 17:40:57 +0100

    change quotation marks for consistency [#17]

--- basics/my_tests.rb
+++ basics/my_tests.rb
@@ -5,11 +5,11 @@ class ArticleTest < Minitest::Test
 
   def test_initialization
     timebefore = Time.now
-    @article = Article.new("title", "body", "author")
+    @article = Article.new('title', 'body', 'author')
     timeafter = Time.now
-    assert_equal "title", @article.title
-    assert_equal "body", @article.body
-    assert_equal "author", @article.author
+    assert_equal 'title', @article.title
+    assert_equal 'body', @article.body
+    assert_equal 'author', @article.author
     assert timebefore <= @article.created_at
     assert timeafter >= @article.created_at
     assert_equal 0, @article.likes
@@ -17,33 +17,33 @@ class ArticleTest < Minitest::Test
   end
 
   def test_initialization_with_anonymous_author
-    @article = Article.new("title", "body")
-    assert_equal "title", @article.title
-    assert_equal "body", @article.body
+    @article = Article.new('title', 'body')
+    assert_equal 'title', @article.title
+    assert_equal 'body', @article.body
     assert_nil @article.author
   end
 
   def test_liking
-    @article = Article.new("title", "body")
+    @article = Article.new('title', 'body')
     @article.like!
     assert_equal 1, @article.likes
   end
 
   def test_disliking
-    @article = Article.new("title", "body")
+    @article = Article.new('title', 'body')
     @article.dislike!
     assert_equal 1, @article.dislikes
   end
 
   def test_points
-    @article = Article.new("title", "body")
+    @article = Article.new('title', 'body')
     assert_equal @article.likes - @article.dislikes, @article.points
   end
 
   def test_long_lines
-    @article_80 = Article.new("title", 'a'*80)
+    @article_80 = Article.new('title', 'a'*80)
     assert_empty @article_80.long_lines
-    @article_81 = Article.new("title", 'a'*81)
+    @article_81 = Article.new('title', 'a'*81)
     assert_equal 1, @article_81.long_lines.count
   end
 
@@ -63,20 +63,20 @@ class ArticleTest < Minitest::Test
   end
 
   def test_length
-    @article = Article.new("title", "body")
+    @article = Article.new('title', 'body')
     assert_equal 4, @article.length
   end
 
   def test_votes
-    @article = Article.new("title", "body")
+    @article = Article.new('title', 'body')
     assert_equal @article.likes + @article.dislikes, @article.votes
   end
 
   def test_contain
-    @article = Article.new("title", "body")
-    assert_equal true, @article.contain?("bo")
-    assert_equal false, @article.contain?("asdsa")
-    assert_equal true, @article.contain?("dy")
+    @article = Article.new('title', 'body')
+    assert_equal true, @article.contain?('bo')
+    assert_equal false, @article.contain?('asdsa')
+    assert_equal true, @article.contain?('dy')
   end
 end
 
