8b342aac4d2636d6ed3f626fa0b838b253d36ddc
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-24 16:56:08 +0100

    add test for best author [#19]

--- basics/my_tests.rb
+++ basics/my_tests.rb
@@ -196,6 +196,8 @@ class WebPageTest < Minitest::Test
   end
 
   def test_best_author
+    @page.instance_variable_set(:@articles, [@article1, @article2, @article3])
+    assert_equal 'author', @page.best_author
   end
 
   def test_search
