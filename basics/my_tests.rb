bc29ec735dd90688b6e690505c5a8e6851cb4db0
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-24 16:56:20 +0100

    add test for search [#19]

--- basics/my_tests.rb
+++ basics/my_tests.rb
@@ -201,5 +201,8 @@ class WebPageTest < Minitest::Test
   end
 
   def test_search
+    article4 = Article.new('title4', 'body')
+    @page.instance_variable_set(:@articles, [@article1, @article2, @article3, article4])
+    assert_equal [ article4 ], @page.search('bo')
   end
 end
