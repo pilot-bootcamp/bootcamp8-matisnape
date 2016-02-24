2c0eea4dc84d0b6e6793e464b81dcd1e6e87d83a
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-24 16:54:29 +0100

    add test for worst article [#19]

--- basics/my_tests.rb
+++ basics/my_tests.rb
@@ -165,6 +165,8 @@ class WebPageTest < Minitest::Test
   end
 
   def test_worst_article
+    @page.instance_variable_set(:@articles, [@article1, @article2, @article3])
+    assert_equal @article1, @page.worst_articles.first
   end
 
   def test_worst_article_exception_when_no_articles_can_be_found
