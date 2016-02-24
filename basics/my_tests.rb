8e0968330b076dcafb3682edfe2940e748607b62
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-24 16:53:25 +0100

    add test for best article [#19]

--- basics/my_tests.rb
+++ basics/my_tests.rb
@@ -149,6 +149,8 @@ class WebPageTest < Minitest::Test
   end
 
   def test_best_article
+    @page.instance_variable_set(:@articles, [@article1, @article2, @article3])
+    assert_equal @article3, @page.best_articles.first
   end
 
   def test_best_article_exception_when_no_articles_can_be_found
