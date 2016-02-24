c739e13b2ffe973074e34fd404c9c21178756189
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-24 10:12:43 +0100

    refactor test for long lines - again [#17]
    still doesn’t work :[

--- basics/my_tests.rb
+++ basics/my_tests.rb
@@ -41,10 +41,9 @@ class ArticleTest < Minitest::Test
   end
 
   def test_long_lines
-    @article_80 = Article.new('title', 'a'*80)
-    assert_empty @article_80.long_lines
-    @article_81 = Article.new('title', 'a'*81)
-    assert_equal 1, @article_81.long_lines.count
+    @article.body = 'a'*80 + '\n' + 'b'*81 + '\n' + 'c'*15
+    assert_equal 1, @article.long_lines.count
+    assert_equal 'b'*81, @article.long_lines.first
   end
 
   def test_truncate
