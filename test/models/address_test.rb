255d47d83455185a08f198e846e372146e51cb43
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-25 11:44:53 +0100

    add tests for validations [#27]

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_validate_presence_city
+    assert Address.create(street: 'Poznanska', zip_code: '12-123').invalid?, "can't be blank"
+    assert Address.create(city: 'Poznan', street: 'Poznanska', zip_code: '12-123').valid?
+  end
+
+  def test_validate_presence_street
+    assert Address.create(city: 'Poznan', zip_code: '12-123').invalid?, "can't be blank"
+    assert Address.create(city: 'Poznan', street: 'Poznanska', zip_code: '12-123').valid?
+  end
+
+  def test_validate_presence_zip_code
+    assert Address.create(city: 'Poznan', street: 'Poznanska').invalid?, "can't be blank"
+    assert Address.create(city: 'Poznan', street: 'Poznanska', zip_code: '12-123').valid?
+  end
+
+  def test_validate_format_zip_code
+    assert Address.create(city: 'Poznan', street: 'Poznanska', zip_code: '12123').invalid?, "Zip code format should be XX-XXX"
+    assert Address.create(city: 'Poznan', street: 'Poznanska', zip_code: '1-1234').invalid?, "Zip code format should be XX-XXX"
+    assert Address.create(city: 'Poznan', street: 'Poznanska', zip_code: '12-123-').invalid?, "Zip code format should be XX-XXX"
+    assert Address.create(city: 'Poznan', street: 'Poznanska', zip_code: '12-123').valid?
+  end
 end
