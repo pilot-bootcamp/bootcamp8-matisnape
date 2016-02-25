764307f3dfcfaf6e233d8c68f5ed3e511e907fc9
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-25 12:21:42 +0100

    add validation tests for person [#29]

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @person = people(:one)
+  end
+
+  test "Valid if all info is present" do
+    assert @person.valid?
+  end
+
+  test "Invalid if there's no first name" do
+    @person.first_name = ""
+    assert_not @person.valid?, "can't be blank"
+  end
 end
