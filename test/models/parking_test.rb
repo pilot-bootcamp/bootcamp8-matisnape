532698d2ef124428f19a0f1a087f230640bfbba9
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-25 15:42:50 +0100

    remove redundant msg from tests [#30]

--- test/models/parking_test.rb
+++ test/models/parking_test.rb
@@ -11,41 +11,41 @@ class ParkingTest < ActiveSupport::TestCase
 
   test "Invalid without address" do
     @parking.address = nil
-    assert_not @parking.valid?, "can't be blank"
+    assert_not @parking.valid?
   end
 
   test "Invalid without number of places" do
     @parking.places = nil
-    assert_not @parking.valid?, "can't be blank"
+    assert_not @parking.valid?
   end
 
   test "Invalid without hour price" do
     @parking.hour_price = nil
-    assert_not @parking.valid?, "can't be blank"
+    assert_not @parking.valid?
   end
 
   test "Invalid without day price" do
     @parking.day_price = nil
-    assert_not @parking.valid?, "can't be blank"
+    assert_not @parking.valid?
   end
 
   test "Invalid without owner" do
     @parking.owner = nil
-    assert_not @parking.valid?, "can't be blank"
+    assert_not @parking.valid?
   end
 
   test "Invalid if kind is different from select options" do
     @parking.kind = ""
-    assert_not @parking.valid?, "%{value} is not a valid kind"
+    assert_not @parking.valid?
   end
 
   test "Invalid if hour price is not a number" do
     @parking.hour_price = "rand"
-    assert_not @parking.valid?, "is not a number"
+    assert_not @parking.valid?
   end
 
   test "Invalid if day price is not a number" do
     @parking.day_price = ""
-    assert_not @parking.valid?, "is not a number"
+    assert_not @parking.valid?
   end
 end
