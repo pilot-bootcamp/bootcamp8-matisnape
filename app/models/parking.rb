b103da5f267460c522349b88c8cb5e7bef698541
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-25 12:47:34 +0100

    add validations for parking [#30]

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -2,4 +2,10 @@ class Parking < ActiveRecord::Base
   has_one :address
   has_many :place_rents
   belongs_to :owner, class_name: "Person"
+
+  validates :place, :hour_price, :day_price, presence: true
+  validates :kind, inclusion: { in: %w(outdoor indoor private street),
+    message: "%{value} is not a valid kind" }
+  validates :hour_price, :day_price, format: { with: /\d/,
+    message: "Only allows numbers" }
 end
