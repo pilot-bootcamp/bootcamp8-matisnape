7b07a5f6372ef510bd36b7cd5072f3791fc8f8fe
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-25 15:12:46 +0100

    add tests for place rents [#31]

--- test/models/place_rent_test.rb
+++ test/models/place_rent_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class PlaceRentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @place_rent = place_rents(:one)
+  end
+
+  test "Valid with all data present" do
+    assert @place_rent.valid?
+  end
+
+  test "Invalid without start date" do
+    @place_rent.starts_at = nil
+    assert_not @place_rent.valid?, "can't be blank"
+  end
+
+  test "Invalid without end date" do
+    @place_rent.ends_at = nil
+    assert_not @place_rent.valid?, "can't be blank"
+  end
+
+  test "Invalid without parking" do
+    @place_rent.parking = nil
+    assert_not @place_rent.valid?, "can't be blank"
+  end
+
+  test "Invalid without car" do
+    @place_rent.car = nil
+    assert_not @place_rent.valid?, "can't be blank"
+  end
 end
