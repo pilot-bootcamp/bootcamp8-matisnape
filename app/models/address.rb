c90efb28222e0f2211702d1baeb7bea1f41669ed
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-25 11:44:33 +0100

    add validations for address format [#27]

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,6 @@
 class Address < ActiveRecord::Base
   belongs_to :parking
+  validates :city, :street, :zip_code, presence: true
+  validates :zip_code, format: { with: /\A\d{2}-\d{3}\z/,
+    message: "Zip code format should be XX-XXX" }
 end
