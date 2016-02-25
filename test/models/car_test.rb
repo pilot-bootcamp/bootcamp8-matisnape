1820589c96162959e9b3292df3a0961daf13d059
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-25 12:17:58 +0100

    add validation tests for car [#28]

--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class CarTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @car = cars(:one)
+  end
+
+  test "Valid with all data present" do
+    assert @car.valid?
+  end
+
+  test "Invalid without registration number" do
+    @car.registration_number = ""
+    assert_not @car.valid?, "can't be blank"
+  end
+
+  test "Invalid without model" do
+    @car.model = ""
+    assert_not @car.valid?, "can't be blank"
+  end
+
+  test "Invalid without owner" do
+    @car.owner_id = ""
+    assert_not @car.valid?, "can't be blank"
+  end
 end
