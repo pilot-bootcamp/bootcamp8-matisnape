85e2798cbf7e6b6587e94597771d055f07a9b41b
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-26 16:16:25 +0100

    add methods for creating a new car [#39]

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -8,10 +8,18 @@ class CarsController < ApplicationController
   end
 
   def new
-    @car = Car.new
+    @car = current_person.cars.build
   end
 
   def create
+    @car = current_person.cars.build(car_params)
+    if @car.save
+      flash[:success] = "Car has been saved correctly"
+      redirect_to cars_path
+    else
+      flash.now[:error] = "Cannot create car because of reasons."
+      render new_car_path
+    end
   end
 
   def edit
