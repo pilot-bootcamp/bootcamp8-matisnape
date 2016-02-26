122630495ed220bb4215ffca9890ec36121b6fa7
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-26 16:13:27 +0100

    optimize requests for person's cars

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ActiveRecord::Base
   has_many :parkings, foreign_key: "owner_id"
-  has_many :cars, foreign_key: "owner_id"
+  has_many :cars, foreign_key: "owner_id", inverse_of: :owner
   validates :first_name, presence: true
 end
