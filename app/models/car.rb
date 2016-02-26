f6dc98979147b9091ae5fe202cefe22d1e559269
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-26 16:10:52 +0100

    update car model to accept nested attributes [#39]

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,7 @@
 class Car < ActiveRecord::Base
-  belongs_to :owner, class_name: "Person"
+  belongs_to :owner, class_name: "Person", dependent: :destroy
   has_many :place_rents
+  accepts_nested_attributes_for :owner,
+    allow_destroy: true
   validates :registration_number, :model, :owner, presence: true
 end
