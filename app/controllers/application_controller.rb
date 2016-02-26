ebc9b58d4a60af4e07942d9d842b273ada467c60
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-26 13:19:37 +0100

    set user from seeds as current one [#38]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_person
+
+  private
+
+  def current_person
+    @current_person ||= Person.first
+  end
 end
