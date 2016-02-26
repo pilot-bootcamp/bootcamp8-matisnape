5501a7da64ce53be5db11553563ad18c6905eb41
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-26 16:47:22 +0100

    remove nested attributes finally [#39]

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,5 @@
 class Car < ActiveRecord::Base
   belongs_to :owner, class_name: "Person"
   has_many :place_rents
-  accepts_nested_attributes_for :owner,
-    allow_destroy: true
   validates :registration_number, :model, :owner, presence: true
 end
