5c2d442baa7f81fac62d61766e73acb91fe5af70
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-26 16:46:29 +0100

    move dependence parameter to a proper model [#39]

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,7 @@
 class Person < ActiveRecord::Base
   has_many :parkings, foreign_key: "owner_id"
-  has_many :cars, foreign_key: "owner_id", inverse_of: :owner
+  has_many :cars, foreign_key: "owner_id",
+    inverse_of: :owner,
+    dependent: :destroy
   validates :first_name, presence: true
 end
