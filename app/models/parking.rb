4c8e0ffbdadbaf2bbefdcbca6e3571e68eee402b
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-26 11:58:10 +0100

    change parking model to accept nested attributes [#37]

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -2,8 +2,10 @@ class Parking < ActiveRecord::Base
   belongs_to :address
   has_many :place_rents
   belongs_to :owner, class_name: "Person"
+  accepts_nested_attributes_for :address,
+    allow_destroy: true
 
-  validates :places, :hour_price, :day_price, presence: true
+  validates :places, :hour_price, :day_price, :address, presence: true
   validates :kind, inclusion: { in: %w(outdoor indoor private street),
     message: "%{value} is not a valid kind" }
   validates :hour_price, :day_price, numericality: true
