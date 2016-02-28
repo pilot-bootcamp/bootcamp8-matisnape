1a000d9cea2d8c52cec6999f29ef8e925dcf095c
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-28 21:57:46 +0100

    add methods fro showing and creating a new place rent [#41]

--- app/controllers/place_rents_controller.rb
+++ app/controllers/place_rents_controller.rb
@@ -4,6 +4,26 @@ class PlaceRentsController < ApplicationController
   end
 
   def show
-    @place_rent = current_person.place_rents.find params[:id]
+    @place_rent = current_person.place_rents.find(params[:id])
+  end
+
+  def new
+    @place_rent = current_person.place_rents.build
+  end
+
+  def create
+    @place_rent = current_person.place_rents.build(place_rent_params)
+    if @place_rent.save
+      flash[:success] = "Place rent has been saved correctly"
+      redirect_to place_rents_path
+    else
+      flash.now[:error] = "Cannot create place_rent because of reasons."
+      render new_place_rent_path
+    end
+  end
+
+  private
+  def place_rent_params
+    params.require(:place_rent).permit(:starts_at, :ends_at)
   end
 end
