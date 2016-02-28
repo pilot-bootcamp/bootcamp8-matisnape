15b31c649abcfeb6ee63216918dc489526afc559
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-28 22:18:38 +0100

    add method for displaying car details and update place rents view [#40]

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,8 @@ class Car < ActiveRecord::Base
   belongs_to :owner, class_name: "Person"
   has_many :place_rents
   validates :registration_number, :model, :owner, presence: true
+
+  def display_car
+    "#{model} #{registration_number}"
+  end
 end
