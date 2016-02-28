59516ddc6b9b91ca41ac7eca69a265c335f76f8e
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-28 22:18:12 +0100

    add method for displaying a whole parking address and update view [#40]

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,8 @@ class Address < ActiveRecord::Base
   validates :city, :street, :zip_code, presence: true
   validates :zip_code, format: { with: /\A\d{2}-\d{3}\z/,
     message: "Zip code format should be XX-XXX" }
+
+  def display_full_address
+    "#{zip_code} #{city}, #{street}"
+  end
 end
