6ea75d6f1f149781779800743d414cdcde6fa933
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-29 16:30:01 +0100

    adding test for adding a parking [#48]

--- test/integration/parkings_test.rb
+++ test/integration/parkings_test.rb
@@ -20,5 +20,25 @@ class ParkingsTest < ActionDispatch::IntegrationTest
     assert has_content? "Parking Poznań, Św. Marcin"
     assert_not has_content? "Warszawa 50 2.9 12.99"
   end
+
+  test "user adds a new parking successfully with valid data" do
+    click_link "Add new parking"
+    fill_in "Enter city:", with: "Lublin"
+    fill_in "Enter street", with: "Poznańska"
+    fill_in "Enter zipcode", with: "62-024"
+    fill_in "Enter number of places available:", with: "123"
+    fill_in "Enter price per hour:", with: 56
+    fill_in "Enter price per day:", with: 78
+    select "private", from: "Enter kind:"
+    click_on "Submit"
+    assert has_content? "Parking has been saved correctly"
+    assert has_content? "Lublin 123 56.0 78.0"
+  end
+
+  test "user fails to add a new parking with invalid data" do
+    click_link "Add new parking"
+    click_on "Submit"
+    assert has_content? "Cannot create parking because of reasons."
+  end
   end
 end
