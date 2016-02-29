e8c40a6822702ce430e447f514bd2dcf3fb63d16
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-29 16:17:59 +0100

    update place rents controller to have safer methods [#41]

--- app/controllers/place_rents_controller.rb
+++ app/controllers/place_rents_controller.rb
@@ -8,23 +8,28 @@ class PlaceRentsController < ApplicationController
   end
 
   def new
-    @place_rent = current_person.place_rents.build(parking_id:params[:parking_id])
+    @place_rent = parking.place_rents.build
   end
 
   def create
-    @place_rent = current_person.place_rents.build(place_rent_params)
+    @place_rent = parking.place_rents.build(place_rent_params)
+    @place_rent.car = current_person.cars.find(params[:place_rent][:car_id])
     if @place_rent.save
       flash[:success] = "Place rent has been saved correctly"
       redirect_to place_rents_path
     else
       flash.now[:error] = "Cannot create place_rent because of reasons."
-      render new_place_rent_path
+      render new_parking_place_rent_path
     end
   end
 
   private
 
   def place_rent_params
-    params.require(:place_rent).permit(:starts_at, :ends_at, :car_id, :parking_id)
+    params.require(:place_rent).permit(:starts_at, :ends_at)
+  end
+
+  def parking
+    @parking = Parking.find(params[:parking_id])
   end
 end
