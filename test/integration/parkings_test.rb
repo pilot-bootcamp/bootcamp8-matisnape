116e1bde62c90caa81f9ce527270e8b934446844
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-02-29 16:29:43 +0100

    add tests for viewing parkings and a single parking [#48]

--- test/integration/parkings_test.rb
+++ test/integration/parkings_test.rb
@@ -1,10 +1,24 @@
 require 'test_helper'
 
 class ParkingsTest < ActionDispatch::IntegrationTest
-  include Capybara::DSL
+
+  setup do
+    visit '/parkings'
+  end
 
   test "user opens parkings index" do
-    visit '/parkings'
-    assert has_content? 'Parkings'
+    assert has_content? "Parkings"
+  end
+
+  test "user sees parkings" do
+    assert has_content? "Poznań 100 3.5 20.99"
+    assert has_content? "Warszawa 50 2.9 12.99"
+  end
+
+  test "user sees particular parking details when clicking Show" do
+    click_link "Show", match: :first
+    assert has_content? "Parking Poznań, Św. Marcin"
+    assert_not has_content? "Warszawa 50 2.9 12.99"
+  end
   end
 end
