2c73388c0b4d155e6cd37e18f67471e4443ac72e
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-01 12:59:35 +0100

    add method for counting place rent's price [#51]

--- app/models/place_rent.rb
+++ app/models/place_rent.rb
@@ -3,4 +3,10 @@ class PlaceRent < ActiveRecord::Base
   belongs_to :parking
 
   validates :starts_at, :ends_at, :parking, :car, presence: true
+
+  def price
+    total_hours = ((ends_at - starts_at)/3600).ceil
+    days, hours = total_hours.divmod(24)
+    parking.day_price * days + parking.hour_price * hours
+  end
 end
