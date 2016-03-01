20058e74d82529267ab5429c03a1f5c2bdc32e9b
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-01 18:50:19 +0100

    add tests for displaying full name [#60]

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -13,4 +13,13 @@ class PersonTest < ActiveSupport::TestCase
     @person.first_name = ""
     assert_not @person.valid?, "can't be blank"
   end
+
+  test "Display full name of current user" do
+    assert_equal "Katarzyna Iksińska", @person.fullname
+  end
+
+  test "Display only first name of current user if last name is empty" do
+    @person.last_name = nil
+    assert_equal "Katarzyna", @person.fullname
+  end
 end
