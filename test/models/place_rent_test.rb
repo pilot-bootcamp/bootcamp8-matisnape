fcc419b9b2db1cf230d2339d2aac29b492913b85
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-01 13:00:06 +0100

    add unit tests for price method [#51]

--- test/models/place_rent_test.rb
+++ test/models/place_rent_test.rb
@@ -28,4 +28,27 @@ class PlaceRentTest < ActiveSupport::TestCase
     @place_rent.car = nil
     assert_not @place_rent.valid?
   end
+
+  test "Price is 0 if start date and end date are equal" do
+    @place_rent.ends_at = @place_rent.starts_at
+    assert_equal 0, @place_rent.price.to_i
+  end
+
+  test "Price is counted for every started hour" do
+    @place_rent.starts_at = DateTime.new(2015,11,19,8,37)
+    @place_rent.ends_at = DateTime.new(2015,11,19,9,00)
+    assert_equal 3.5, @place_rent.price.to_f
+  end
+
+  test "23 hours with minutes is counted as one day" do
+    @place_rent.starts_at = DateTime.new(2015,11,19,8,37)
+    @place_rent.ends_at = DateTime.new(2015,11,20,8,00)
+    assert_equal 20.99, @place_rent.price.to_f
+  end
+
+  test "24 hours with minutes is counted as one day and one hour" do
+    @place_rent.starts_at = DateTime.new(2015,11,19,8,37)
+    @place_rent.ends_at = DateTime.new(2015,11,20,9,00)
+    assert_equal 24.49, @place_rent.price.to_f
+  end
 end
