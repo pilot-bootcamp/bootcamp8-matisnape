cc7b10d1aed4fd4d8823b72bdd6d048b0f5cb586
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-01 10:06:27 +0100

    add describe to cars test [#50]

--- test/integration/cars_test.rb
+++ test/integration/cars_test.rb
@@ -5,64 +5,72 @@ class CarsTest < ActionDispatch::IntegrationTest
     visit cars_path
   end
 
-  test "user opens cars index" do
-    assert has_content? "Cars"
+  describe "viewing cars" do
+    test "user opens cars index" do
+      assert has_content? "Cars"
+    end
+
+    test "user sees cars list" do
+      assert has_content? "PZ12345 Fiat Panda"
+      assert has_content? "WE65432 Ford Escort"
+    end
+
+    test "user sees particular car details when clicking Show" do
+      click_link "Show", match: :first
+      assert has_content? "PZ12345 Fiat Panda"
+      assert_not has_content? "WE65432 Ford Escort"
+    end
   end
 
-  test "user sees cars list" do
-    assert has_content? "PZ12345 Fiat Panda"
-    assert has_content? "WE65432 Ford Escort"
+  describe "adding a new car" do
+    before { click_link "Add new car" }
+
+    test "user adds a new car successfully with valid data" do
+      fill_in "Enter registration number:", with: "WAW12345"
+      fill_in "Enter model:", with: "Syrena Bosto"
+      click_on "Submit"
+      assert has_content? "Car has been saved correctly"
+      assert has_content? "WAW12345 Syrena Bosto"
+    end
+
+    test "user fails to add a new car with invalid data" do
+      click_on "Submit"
+      assert has_content? "Cannot create car because of reasons."
+    end
   end
 
-  test "user sees particular car details when clicking Show" do
-    click_link "Show", match: :first
-    assert has_content? "PZ12345 Fiat Panda"
-    assert_not has_content? "WE65432 Ford Escort"
+  describe "editing a car" do
+    before { click_link "Edit", match: :first }
+
+    test "user edits a car successfully with valid data" do
+      fill_in "Enter registration number:", with: "LOL12345"
+      fill_in "Enter model:", with: "Volkswagen Polo"
+      click_on "Submit"
+      assert has_content? "Car has been saved correctly"
+      assert_not has_content? "WAW12345 Syrena Bosto"
+      assert has_content? "LOL12345 Volkswagen Polo"
+    end
+
+    test "user fails to edit a car because of invalid data" do
+      fill_in "Enter registration number:", with: ""
+      fill_in "Enter model:", with: ""
+      click_on "Submit"
+      assert has_content? "Cannot update car because of reasons."
+    end
   end
 
-  test "user adds a new car successfully with valid data" do
-    click_link "Add new car"
-    fill_in "Enter registration number:", with: "WAW12345"
-    fill_in "Enter model:", with: "Syrena Bosto"
-    click_on "Submit"
-    assert has_content? "Car has been saved correctly"
-    assert has_content? "WAW12345 Syrena Bosto"
-  end
+  describe "removing a car" do
+    test "user removes a car successfully" do
+      page.accept_confirm { click_link "Remove", match: :first }
+      assert_not has_content? "PZ12345 Fiat Panda"
+      assert has_content? "Car deleted successfully"
+    end
 
-  test "user fails to add a new car with invalid data" do
-    click_link "Add new car"
-    click_on "Submit"
-    assert has_content? "Cannot create car because of reasons."
-  end
-
-  test "user edits a car successfully with valid data" do
-    click_link "Edit", match: :first
-    fill_in "Enter registration number:", with: "LOL12345"
-    fill_in "Enter model:", with: "Volkswagen Polo"
-    click_on "Submit"
-    assert has_content? "Car has been saved correctly"
-    assert_not has_content? "WAW12345 Syrena Bosto"
-    assert has_content? "LOL12345 Volkswagen Polo"
-  end
-
-  test "user fails to edit a car because of invalid data" do
-    click_link "Edit", match: :first
-    fill_in "Enter registration number:", with: ""
-    fill_in "Enter model:", with: ""
-    click_on "Submit"
-    assert has_content? "Cannot update car because of reasons."
-  end
-
-  test "user removes a car successfully" do
-    page.accept_confirm { click_link "Remove", match: :first }
-    assert_not has_content? "PZ12345 Fiat Panda"
-    assert has_content? "Car deleted successfully"
-  end
-
-  test "user fails to remove a car" do
-    page.dismiss_confirm { click_link "Remove", match: :first }
-    assert has_content? "PZ12345 Fiat Panda"
-    assert has_content? "WE65432 Ford Escort"
-    assert_not has_content? "Car deleted successfully"
+    test "user fails to remove a car" do
+      page.dismiss_confirm { click_link "Remove", match: :first }
+      assert has_content? "PZ12345 Fiat Panda"
+      assert has_content? "WE65432 Ford Escort"
+      assert_not has_content? "Car deleted successfully"
+    end
   end
 end
