c8db81ed5fd95c10e8a95731c14f68ff2c76d0f1
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-01 16:38:56 +0100

    add helper for changing deleted parking's address to a string [#53]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def parking_address(parking)
+    if parking.present?
+      link_to parking.address.display_full_address, parking_path(parking)
+    else
+      "Deleted parking"
+    end
+  end
 end
