b35903fe025c4e54cb8b07e694768ab094685d83
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-01 16:41:10 +0100

    add callback for closing open place rents [#53]

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -2,11 +2,22 @@ class Parking < ActiveRecord::Base
   belongs_to :address
   has_many :place_rents
   belongs_to :owner, class_name: "Person"
-  accepts_nested_attributes_for :address,
-    allow_destroy: true
+  accepts_nested_attributes_for :address
 
   validates :places, :hour_price, :day_price, :address, :owner, presence: true
   validates :kind, inclusion: { in: %w(outdoor indoor private street),
     message: "%{value} is not a valid kind" }
   validates :hour_price, :day_price, numericality: true
+
+  after_destroy :close_place_rents
+
+  private
+
+  def close_place_rents
+    time = DateTime.now
+    open_place_rents = place_rents.open(time)
+    open_place_rents.each do |open_place_rent|
+      open_place_rent.update(ends_at: time)
+    end
+  end
 end
