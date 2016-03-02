66737a9c43d97aa14a451d56d62a4843acc40cd5
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-02 14:49:35 +0100

    update tests [#61]

--- test/integration/sign_in_test.rb
+++ test/integration/sign_in_test.rb
@@ -2,11 +2,18 @@ require 'test_helper'
 
 class SignInTest < ActionDispatch::IntegrationTest
 
-  test "user can log in" do
+  test "user can log in and the user's name is displayed on the page" do
     visit '/session/new'
     fill_in "Email", with: "anna.nowak@netguru.pl"
     fill_in "Password", with: "password"
     click_on "Submit"
     assert has_content? "Logged in as: Anna"
+    assert_not has_content? "Log in"
+  end
+
+  test "non-logged in user doesn't see any name on the page" do
+    visit '/session/new'
+    assert has_content? "Log in"
+    assert_not has_content? "Logged in as:"
   end
 end
