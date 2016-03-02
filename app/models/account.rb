ba093abf1d1606ed0cfc1e3f589c0a3dfddd391b
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-02 18:13:13 +0100

    update account model [#63]

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,11 @@
 class Account < ActiveRecord::Base
   belongs_to :person
+  has_secure_password
 
   validates :email, :password, presence: true
+
+  def self.authenticate(email, password)
+    user = Account.find_by(email: email)
+    user.blank? ? false : user.authenticate(password)
+  end
 end
