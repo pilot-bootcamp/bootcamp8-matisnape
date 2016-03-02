c8c4d8745acf5bd856d676f24d048f9aa10abfb3
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-02 22:47:56 +0100

    refactor tests for parkings [#64]

--- test/integration/parkings_test.rb
+++ test/integration/parkings_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 
 class ParkingsTest < ActionDispatch::IntegrationTest
-  setup do
-    visit parkings_path
-  end
-
   describe "viewing parkings" do
+    setup do
+      visit parkings_path
+    end
+
     it "user opens parkings index" do
       assert has_content? "Parkings"
     end
@@ -27,70 +27,76 @@ class ParkingsTest < ActionDispatch::IntegrationTest
     end
   end
 
-  describe "adding a new parking" do
-    before { click_link "Add new parking" }
+  describe "logged user" do
+    before { sign_in("anna.nowak@netguru.pl", "password") }
 
-    test "user adds a new parking successfully with valid data" do
-      fill_in "Enter city:", with: "Lublin"
-      fill_in "Enter street", with: "Poznańska"
-      fill_in "Enter zipcode", with: "62-024"
-      fill_in "Enter number of places available:", with: "123"
-      fill_in "Enter price per hour:", with: 56
-      fill_in "Enter price per day:", with: 78
-      select "private", from: "Enter kind:"
-      click_on "Submit"
-      assert has_content? "Parking has been saved correctly"
-      assert has_content? "Lublin 123 56.0 78.0"
+    describe "adding a new parking" do
+      before { click_link "Add new parking" }
+
+      test "user adds a new parking successfully with valid data" do
+        fill_in "Enter city:", with: "Lublin"
+        fill_in "Enter street", with: "Poznańska"
+        fill_in "Enter zipcode", with: "62-024"
+        fill_in "Enter number of places available:", with: "123"
+        fill_in "Enter price per hour:", with: 56
+        fill_in "Enter price per day:", with: 78
+        select "private", from: "Enter kind:"
+        click_on "Submit"
+        assert has_content? "Parking has been saved correctly"
+        assert has_content? "Lublin 123 56.0 78.0"
+      end
+
+      test "user fails to add a new parking with invalid data" do
+        click_on "Submit"
+        assert has_content? "Cannot create parking because of reasons."
+      end
     end
 
-    test "user fails to add a new parking with invalid data" do
-      click_on "Submit"
-      assert has_content? "Cannot create parking because of reasons."
-    end
-  end
+    describe "editing a parking" do
+      before { click_link "Edit", match: :first }
 
-  describe "editing a parking" do
-    before { click_link "Edit", match: :first }
+      test "user edits a parking successfully" do
+        fill_in "Enter city:", with: "Lublin"
+        fill_in "Enter street", with: "Poznańska"
+        fill_in "Enter zipcode", with: "62-024"
+        fill_in "Enter number of places available:", with: "123"
+        fill_in "Enter price per hour:", with: 56
+        fill_in "Enter price per day:", with: 78
+        select "private", from: "Enter kind:"
+        click_on "Submit"
+        assert has_content? "Parking has been saved correctly"
+        assert has_content? "Lublin 123 56.0 78.0"
+        assert_not has_content? "Poznań 100 3.5 20.99"
+      end
 
-    test "user edits a parking successfully" do
-      fill_in "Enter city:", with: "Lublin"
-      fill_in "Enter street", with: "Poznańska"
-      fill_in "Enter zipcode", with: "62-024"
-      fill_in "Enter number of places available:", with: "123"
-      fill_in "Enter price per hour:", with: 56
-      fill_in "Enter price per day:", with: 78
-      select "private", from: "Enter kind:"
-      click_on "Submit"
-      assert has_content? "Parking has been saved correctly"
-      assert has_content? "Lublin 123 56.0 78.0"
-      assert_not has_content? "Poznań 100 3.5 20.99"
-    end
-
-    test "user fails to edit a parking because of invalid data" do
-      fill_in "Enter city:", with: ""
-      fill_in "Enter street", with: ""
-      fill_in "Enter zipcode", with: ""
-      fill_in "Enter number of places available:", with: ""
-      fill_in "Enter price per hour:", with: nil
-      fill_in "Enter price per day:", with: nil
-      select "private", from: "Enter kind:"
-      click_on "Submit"
-      assert has_content? "Cannot update parking because of reasons."
-    end
-  end
-
-  describe "removing a parking" do
-    test "user removes a parking successfully" do
-      page.accept_confirm { click_link "Remove", match: :first }
-      assert_not has_content? "Poznań 100 3.5 20.99"
-      assert has_content? "Parking deleted successfully"
+      test "user fails to edit a parking because of invalid data" do
+        fill_in "Enter city:", with: ""
+        fill_in "Enter street", with: ""
+        fill_in "Enter zipcode", with: ""
+        fill_in "Enter number of places available:", with: ""
+        fill_in "Enter price per hour:", with: nil
+        fill_in "Enter price per day:", with: nil
+        select "private", from: "Enter kind:"
+        click_on "Submit"
+        assert has_content? "Cannot update parking because of reasons."
+      end
     end
 
-    test "user fails to remove a parking" do
-      page.dismiss_confirm { click_link "Remove", match: :first }
-      assert has_content? "Poznań 100 3.5 20.99"
-      assert has_content? "Warszawa 50 2.9 12.99"
-      assert_not has_content? "Parking deleted successfully"
+    describe "removing a parking" do
+      before { visit parkings_path }
+
+      test "user removes a parking successfully" do
+        page.accept_confirm { click_link "Remove", match: :first }
+        assert_not has_content? "Poznań 100 3.5 20.99"
+        assert has_content? "Parking deleted successfully"
+      end
+
+      test "user fails to remove a parking" do
+        page.dismiss_confirm { click_link "Remove", match: :first }
+        assert has_content? "Poznań 100 3.5 20.99"
+        assert has_content? "Warszawa 50 2.9 12.99"
+        assert_not has_content? "Parking deleted successfully"
+      end
     end
   end
 end
