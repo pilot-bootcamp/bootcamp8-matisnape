505c63736b17d6ef76239d3939ac8a2acffb3e82
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-02 16:29:09 +0100

    update seeds #61]

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,9 @@ Address.delete_all
 Car.delete_all
 Parking.delete_all
 PlaceRent.delete_all
+Account.delete_all
 
-person1 = Person.create(first_name: 'Katarzyna', last_name: 'Iksińska')
+person1 = Person.create(first_name: 'Anna', last_name: 'Nowak')
 person2 = Person.create(first_name: 'Janusz', last_name: 'Ygrekowski')
 car1 = Car.create(registration_number: 'PZ12345', model: 'Fiat Punto', owner: person1)
 car2 = Car.create(registration_number: 'WE65432', model: 'Ford Escort', owner: person2)
@@ -22,3 +23,5 @@ parking2 = Parking.create(places: 50, kind: 'private', hour_price: 2.9, day_pric
 place_rent1 = PlaceRent.create(starts_at: '2016-01-24 08:15:34', ends_at: '2016-01-24 18:15:34', parking: parking1, car: car1)
 place_rent2 = PlaceRent.create(starts_at: '2016-02-24 10:15:34', ends_at: '2016-02-24 18:15:34', parking: parking2, car: car2)
 place_rent3 = PlaceRent.create(starts_at: '2016-01-24 10:15:34', ends_at: '2017-02-24 18:15:34', parking: parking1, car: car1)
+account1 = Account.create(email: 'anna.nowak@netguru.pl', password: 'password', person: person1)
+account2 = Account.create(email: 'admin@example.com', password: 'password123', person: person2)
