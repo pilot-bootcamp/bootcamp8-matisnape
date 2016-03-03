3d0ec904730b442cb83ecd4f763527872528bfe4
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-03 10:24:43 +0100

    add integration tests for redirects [#65]

--- test/integration/sign_in_sign_out_test.rb
+++ test/integration/sign_in_sign_out_test.rb
@@ -11,14 +11,32 @@ class SignInSignOutTest < ActionDispatch::IntegrationTest
       fill_in "Email:", with: "anna.nowak@netguru.pl"
       fill_in "Password:", with: "password"
       click_button "Sign in"
+      assert has_content? "You have successfully logged in."
       assert has_content? "Logged in as: Anna Nowak"
       assert_not has_content? "You're not logged in"
+      assert_equal current_path, root_path
     end
 
     test "non-logged in user doesn't see any name on the page" do
       visit '/session/new'
       assert has_content? "You're not logged in"
       assert_not has_content? "Logged in as:"
+    end
+
+    describe "from specific location" do
+      test "from cars path redirects back to car path" do
+        visit cars_path
+        sign_in("anna.nowak@netguru.pl", "password")
+        assert has_content? "You have successfully logged in."
+        assert_equal current_path, cars_path
+      end
+
+      test "from new parking path redirects back to new parking path" do
+        visit new_parking_path
+        sign_in("anna.nowak@netguru.pl", "password")
+        assert has_content? "You have successfully logged in."
+        assert_equal current_path, new_parking_path
+      end
     end
   end
 
