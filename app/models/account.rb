7215f6b5cc26eb9d8a6dd14cf85f4c419d29cfd0
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-03 12:12:34 +0100

    validate email presence and accept nested attributes for person [#66]

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,8 +1,9 @@
 class Account < ActiveRecord::Base
   belongs_to :person
   has_secure_password
+  accepts_nested_attributes_for :person
 
-  validates :email, :password, presence: true
+  validates :email, presence: true
 
   def self.authenticate(email, password)
     user = Account.find_by(email: email)
