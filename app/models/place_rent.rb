82941bca5ef544d9554efc9d063d5ed182084efa
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-03 14:51:09 +0100

    add uuid generator to place rent model [#70]

--- app/models/place_rent.rb
+++ app/models/place_rent.rb
@@ -5,6 +5,7 @@ class PlaceRent < ActiveRecord::Base
   validates :starts_at, :ends_at, :parking, :car, presence: true
 
   before_create :calculate_price
+  before_create :generate_uuid, unless: :uuid?
 
   scope :open, -> (time) { where("? BETWEEN starts_at AND ends_at", time) }
 
@@ -12,5 +13,13 @@ class PlaceRent < ActiveRecord::Base
     total_hours = ((ends_at - starts_at) / 3600).ceil
     days, hours = total_hours.divmod(24)
     self.price = parking.day_price * days + parking.hour_price * hours
+  end
+
+  def generate_uuid
+    self.uuid = SecureRandom.uuid
+  end
+
+  def to_param
+    "#{id}-#{uuid}"
   end
 end
