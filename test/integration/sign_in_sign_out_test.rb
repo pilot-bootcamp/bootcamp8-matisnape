a96c4233c9e705ca2f7ed58457c10b250af2ca1c
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-03 18:44:47 +0100

    fix tests [#73]

--- test/integration/sign_in_sign_out_test.rb
+++ test/integration/sign_in_sign_out_test.rb
@@ -7,7 +7,7 @@ class SignInSignOutTest < ActionDispatch::IntegrationTest
 
   describe "Logging in" do
     test "user can log in and the user's name is displayed on the page" do
-      visit '/session/new'
+      visit login_path
       fill_in "Email:", with: "anna.nowak@netguru.pl"
       fill_in "Password:", with: "password"
       click_button "Sign in"
@@ -18,7 +18,7 @@ class SignInSignOutTest < ActionDispatch::IntegrationTest
     end
 
     test "non-logged in user doesn't see any name on the page" do
-      visit '/session/new'
+      visit login_path
       assert has_content? "You're not logged in"
       assert_not has_content? "Logged in as:"
     end
