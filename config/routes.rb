e34d757959ed1f71f64a3a3fc9277beadd3258a3
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-03 12:32:26 +0100

    add routes aliases [#68]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'parkings#index'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  get    'register'  => 'accounts#new'
 
   resources :parkings do
     resources :place_rents, only: [:new, :create]
