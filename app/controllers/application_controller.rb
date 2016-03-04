2b70301e1bd31326b45aab34db4ebcbf86d93f81
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-04 15:01:22 +0100

    set locale in controller [#78]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :require_login
+  before_filter :set_locale
 
   helper_method :current_person
   helper_method :show_current_controller
 
   private
+
+  def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end
 
   def current_person
     return nil unless session[:current_user_id]
