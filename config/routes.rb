b3e897be53aad2d58b579dcb6699637946459ade
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-04 15:01:30 +0100

    update routes [#78]

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  root 'parkings#index'
-  get    'login'   => 'sessions#new'
-  post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
-  get    'register'  => 'accounts#new'
+  scope "(:locale)", :locale => /en|pl/ do
+    root 'parkings#index'
+    get    'login'   => 'sessions#new'
+    post   'login'   => 'sessions#create'
+    delete 'logout'  => 'sessions#destroy'
+    get    'register'  => 'accounts#new'
+    get '/:locale' => "parkings#index"
 
-  resources :parkings do
-    resources :place_rents, only: [:new, :create]
+    resources :parkings do
+      resources :place_rents, only: [:new, :create]
+    end
+    resources :place_rents, only: [:index, :show]
+    resources :cars
+    resources :accounts, except: [:show, :index]
   end
-  resources :place_rents, only: [:index, :show]
-  resources :cars
-  resources :accounts, except: [:show, :index]
 end
