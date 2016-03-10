490041193245c333eb89e847fe3c9a2176358c62
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-10 23:57:08 +0100

    use translations in place rents tests [#77]

--- test/integration/place_rents_test.rb
+++ test/integration/place_rents_test.rb
@@ -8,20 +8,20 @@ class PlaceRentsTest < ActionDispatch::IntegrationTest
   describe "logged in user" do
     test "user adds a new place rent successfully with valid data" do
       sign_in("anna.nowak@netguru.pl", "password")
-      click_link "Rent a place", match: :first
-      select "Fiat Panda PZ12345", from: "Select car:"
+      click_link t('parkings.rent_place'), match: :first
+      select "Fiat Panda PZ12345", from: t('place_rents.form.select_car')
       select_date_and_time(DateTime.new(2011, 11, 19, 8, 37), from: :place_rent_starts_at)
       select_date_and_time(DateTime.new(2012, 11, 19, 8, 37), from: :place_rent_ends_at)
-      click_on "Submit"
-      assert has_content? "Place rent has been saved correctly"
+      click_on t('buttons.submit')
+      assert has_content? t('place_rents.form.success_msg')
       assert has_content? "2011-11-19 08:37:00 UTC 2012-11-19 08:37:00 UTC 61-248 Poznań, Św. Marcin
         Fiat Panda PZ12345"
     end
 
     test "user without any cars cannot create a place rent" do
       sign_in("nocar@example.com", "password")
-      click_link "Rent a place", match: :first
-      assert has_content? "You can't create a place rent without having any car. Add a car first."
+      click_link t('parkings.rent_place'), match: :first
+      assert has_content? t('place_rents.form.car_first')
       assert_equal new_car_path, current_path
     end
 
@@ -39,7 +39,7 @@ class PlaceRentsTest < ActionDispatch::IntegrationTest
       end
 
       test "user can see a place rents's prices" do
-        assert has_content? "Price of place rent"
+        assert has_content? t('place_rents.price')
         assert has_content? "PZ12345 35.0"
         assert has_content? "WE65432 28.0"
       end
@@ -58,7 +58,7 @@ class PlaceRentsTest < ActionDispatch::IntegrationTest
       end
 
       test "user can see a place rent's price on single place rent page" do
-        assert has_content? "Price of place rent"
+        assert has_content? t('place_rents.price')
         assert has_content? "PZ12345 35.0"
         assert_not has_content? "WE65432 28.0"
       end
@@ -68,18 +68,18 @@ class PlaceRentsTest < ActionDispatch::IntegrationTest
   describe "unlogged user" do
     test "cannot see place rents page" do
       visit place_rents_path
-      assert has_content? "You have to log in first"
+      assert has_content? t('errors.restricted')
     end
 
     test "cannot add a new place rent" do
       visit parkings_path
-      click_link "Rent a place", match: :first
-      assert has_content? "You have to log in first"
+      click_link t('parkings.rent_place'), match: :first
+      assert has_content? t('errors.restricted')
     end
 
     test "cannot view a single place rent" do
       visit place_rent_path(place_rents(:one))
-      assert has_content? "You have to log in first"
+      assert has_content? t('errors.restricted')
     end
   end
 
