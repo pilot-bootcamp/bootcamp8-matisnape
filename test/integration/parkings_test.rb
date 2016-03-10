1a5401532f1a7229f3cad082a84918fe06578343
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-10 23:45:58 +0100

    use translations in parkings tests [#77]

--- test/integration/parkings_test.rb
+++ test/integration/parkings_test.rb
@@ -11,7 +11,7 @@ class ParkingsTest < ActionDispatch::IntegrationTest
     end
 
     it "user opens parkings index" do
-      assert has_content? "Parkings"
+      assert has_content? t('parkings.title')
     end
 
     it "user sees parkings" do
@@ -20,25 +20,25 @@ class ParkingsTest < ActionDispatch::IntegrationTest
     end
 
     it "user sees particular parking details when clicking Show" do
-      click_link "Show", match: :first
-      assert has_content? "Parking Poznań, Św. Marcin"
+      click_link t('crud.show'), match: :first
+      assert has_content? "Parking: Poznań, Św. Marcin"
       assert_not has_content? "Warszawa 50 2.9 12.99"
     end
 
     it "user sees error message when trying to go to a nonexistent parking page" do
       visit parking_path(id: "bjh")
-      assert has_content? "There's no such parking"
+      assert has_content? t('parkings.not_found')
     end
 
     describe "unlogged user" do
       it "cannot view create new parking" do
-        click_link "Add new parking"
-        assert has_content? "You have to log in first"
+        click_link t('parkings.add_new')
+        assert has_content? t('errors.restricted')
       end
 
       it "cannot delete a parking" do
-        click_link "Remove", match: :first
-        assert has_content? "You have to log in first"
+        click_link t('crud.delete'), match: :first
+        assert has_content? t('errors.restricted')
       end
     end
   end
@@ -47,54 +47,54 @@ class ParkingsTest < ActionDispatch::IntegrationTest
     before { sign_in("anna.nowak@netguru.pl", "password") }
 
     describe "adding a new parking" do
-      before { click_link "Add new parking" }
+      before { click_link t('parkings.add_new') }
 
       test "user adds a new parking successfully with valid data" do
-        fill_in "Enter city:", with: "Lublin"
-        fill_in "Enter street", with: "Poznańska"
-        fill_in "Enter zipcode", with: "62-024"
-        fill_in "Enter number of places available:", with: "123"
-        fill_in "Enter price per hour:", with: 56
-        fill_in "Enter price per day:", with: 78
-        select "private", from: "Enter kind:"
-        click_on "Submit"
-        assert has_content? "Parking has been saved correctly"
+        fill_in t('parkings.form.city'), with: "Lublin"
+        fill_in t('parkings.form.street'), with: "Poznańska"
+        fill_in t('parkings.form.zip_code'), with: "62-024"
+        fill_in t('parkings.form.places'), with: "123"
+        fill_in t('parkings.form.price_hour'), with: 56
+        fill_in t('parkings.form.price_day'), with: 78
+        select t('parkings.kinds.private'), from: t('parkings.form.kind')
+        click_on t('buttons.submit')
+        assert has_content? t('parkings.form.success_msg')
         assert has_content? "Lublin 123 56.0 78.0"
       end
 
       test "user fails to add a new parking with invalid data" do
         click_on "Submit"
-        assert has_content? "Cannot create parking because of reasons."
+        assert has_content? t('parkings.form.failed_msg')
       end
     end
 
     describe "editing a parking" do
-      before { click_link "Edit", match: :first }
+      before { click_link t('crud.edit'), match: :first }
 
       test "user edits a parking successfully" do
-        fill_in "Enter city:", with: "Lublin"
-        fill_in "Enter street", with: "Poznańska"
-        fill_in "Enter zipcode", with: "62-024"
-        fill_in "Enter number of places available:", with: "123"
-        fill_in "Enter price per hour:", with: 56
-        fill_in "Enter price per day:", with: 78
-        select "private", from: "Enter kind:"
-        click_on "Submit"
-        assert has_content? "Parking has been saved correctly"
+        fill_in t('parkings.form.city'), with: "Lublin"
+        fill_in t('parkings.form.street'), with: "Poznańska"
+        fill_in t('parkings.form.zip_code'), with: "62-024"
+        fill_in t('parkings.form.places'), with: "123"
+        fill_in t('parkings.form.price_hour'), with: 56
+        fill_in t('parkings.form.price_day'), with: 78
+        select t('parkings.kinds.private'), from: t('parkings.form.kind')
+        click_on t('buttons.submit')
+        assert has_content? t('parkings.form.success_msg')
         assert has_content? "Lublin 123 56.0 78.0"
         assert_not has_content? "Poznań 100 3.5 20.99"
       end
 
       test "user fails to edit a parking because of invalid data" do
-        fill_in "Enter city:", with: ""
-        fill_in "Enter street", with: ""
-        fill_in "Enter zipcode", with: ""
-        fill_in "Enter number of places available:", with: ""
-        fill_in "Enter price per hour:", with: nil
-        fill_in "Enter price per day:", with: nil
-        select "private", from: "Enter kind:"
-        click_on "Submit"
-        assert has_content? "Cannot update parking because of reasons."
+        fill_in t('parkings.form.city'), with: ""
+        fill_in t('parkings.form.street'), with: ""
+        fill_in t('parkings.form.zip_code'), with: ""
+        fill_in t('parkings.form.places'), with: ""
+        fill_in t('parkings.form.price_hour'), with: nil
+        fill_in t('parkings.form.price_day'), with: nil
+        select t('parkings.kinds.private'), from: t('parkings.form.kind')
+        click_on t('buttons.submit')
+        assert has_content? t('parkings.form.failed_update')
       end
     end
 
@@ -102,16 +102,16 @@ class ParkingsTest < ActionDispatch::IntegrationTest
       before { visit parkings_path }
 
       test "user removes a parking successfully" do
-        page.accept_confirm { click_link "Remove", match: :first }
+        page.accept_confirm { click_link t('crud.delete'), match: :first }
         assert_not has_content? "Poznań 100 3.5 20.99"
-        assert has_content? "Parking deleted successfully"
+        assert has_content? t('parkings.form.delete_success')
       end
 
       test "user fails to remove a parking" do
-        page.dismiss_confirm { click_link "Remove", match: :first }
+        page.dismiss_confirm { click_link t('crud.delete'), match: :first }
         assert has_content? "Poznań 100 3.5 20.99"
         assert has_content? "Warszawa 50 2.9 12.99"
-        assert_not has_content? "Parking deleted successfully"
+        assert_not has_content? t('parkings.form.delete_success')
       end
     end
   end
