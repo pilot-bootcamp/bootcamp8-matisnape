a021d5679838b54173b8f7b2a58851c29fae2614
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-10 23:45:47 +0100

    use translations in car tests [#77]

--- test/integration/cars_test.rb
+++ test/integration/cars_test.rb
@@ -12,7 +12,7 @@ class CarsTest < ActionDispatch::IntegrationTest
 
   describe "viewing cars" do
     test "user opens cars index" do
-      assert has_content? "Cars"
+      assert has_content? t('cars.title')
     end
 
     test "user sees cars list" do
@@ -21,66 +21,66 @@ class CarsTest < ActionDispatch::IntegrationTest
     end
 
     test "user sees particular car details when clicking Show" do
-      click_link "Show", match: :first
+      click_link t('crud.show'), match: :first
       assert has_content? "PZ12345 Fiat Panda"
       assert_not has_content? "WE65432 Ford Escort"
     end
 
     test "user sees error message when trying to go to a nonexistent car page" do
       visit car_path(id: "bjh")
-      assert has_content? "There's no such car"
+      assert has_content? t('cars.not_found')
     end
   end
 
   describe "adding a new car" do
-    before { click_link "Add new car" }
+    before { click_link t('cars.add_new') }
 
     test "user adds a new car successfully with valid data" do
-      fill_in "Enter registration number:", with: "WAW12345"
-      fill_in "Enter model:", with: "Syrena Bosto"
-      click_on "Submit"
-      assert has_content? "Car has been saved correctly"
+      fill_in t('cars.form.registration'), with: "WAW12345"
+      fill_in t('cars.form.model'), with: "Syrena Bosto"
+      click_on t('buttons.submit')
+      assert has_content? t('cars.form.success_msg')
       assert has_content? "WAW12345 Syrena Bosto"
     end
 
     test "user fails to add a new car with invalid data" do
-      click_on "Submit"
-      assert has_content? "Cannot create car because of reasons."
+      click_on t('buttons.submit')
+      assert has_content? t('cars.form.failed_msg')
     end
   end
 
   describe "editing a car" do
-    before { click_link "Edit", match: :first }
+    before { click_link t('crud.edit'), match: :first }
 
     test "user edits a car successfully with valid data" do
-      fill_in "Enter registration number:", with: "LOL12345"
-      fill_in "Enter model:", with: "Volkswagen Polo"
-      click_on "Submit"
-      assert has_content? "Car has been saved correctly"
+      fill_in t('cars.form.registration'), with: "LOL12345"
+      fill_in t('cars.form.model'), with: "Volkswagen Polo"
+      click_on t('buttons.submit')
+      assert has_content? t('cars.form.success_msg')
       assert_not has_content? "WAW12345 Syrena Bosto"
       assert has_content? "LOL12345 Volkswagen Polo"
     end
 
     test "user fails to edit a car because of invalid data" do
-      fill_in "Enter registration number:", with: ""
-      fill_in "Enter model:", with: ""
-      click_on "Submit"
-      assert has_content? "Cannot update car because of reasons."
+      fill_in t('cars.form.registration'), with: ""
+      fill_in t('cars.form.model'), with: ""
+      click_on t('buttons.submit')
+      assert has_content? t('cars.form.failed_update')
     end
   end
 
   describe "removing a car" do
     test "user removes a car successfully" do
-      page.accept_confirm { click_link "Remove", match: :first }
+      page.accept_confirm { click_link t('crud.delete'), match: :first }
       assert_not has_content? "PZ12345 Fiat Panda"
-      assert has_content? "Car deleted successfully"
+      assert has_content? t('cars.form.delete_success')
     end
 
     test "user fails to remove a car" do
-      page.dismiss_confirm { click_link "Remove", match: :first }
+      page.dismiss_confirm { click_link t('crud.delete'), match: :first }
       assert has_content? "PZ12345 Fiat Panda"
       assert has_content? "WE65432 Ford Escort"
-      assert_not has_content? "Car deleted successfully"
+      assert_not has_content? t('cars.form.delete_success')
     end
   end
 end
