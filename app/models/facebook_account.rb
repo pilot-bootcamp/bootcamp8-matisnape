cf6f860bea22705acd3150d8f8da5cf558660db1
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-20 18:25:15 +0100

    add method firnd_or_create_for_facebook [#88]

--- app/models/facebook_account.rb
+++ app/models/facebook_account.rb
@@ -1,5 +1,14 @@
 class FacebookAccount < ActiveRecord::Base
   belongs_to :person
+  accepts_nested_attributes_for :person
 
   validates :uid, :person, presence: true
+
+  def self.find_or_create_for_facebook(auth)
+    user = FacebookAccount.find_by(uid: auth['uid'])
+    if user.blank?
+      user = FacebookAccount.create(uid: auth['uid'])
+      person = Person.create(first_name: auth['info']['first_name'], last_name: auth['info']['last_name'])
+    end
+  end
 end
