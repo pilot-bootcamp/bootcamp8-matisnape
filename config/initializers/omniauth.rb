8d3386f7a8989cc10621c09343d4967b79b481b6
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-20 23:17:34 +0100

    info_fields, not scope [#89]

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], scope: 'email,first_name,last_name'
+  provider :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], info_fields: 'email, first_name, last_name'
 end
