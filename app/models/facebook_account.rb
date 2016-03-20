eebb7522f5982dd283895bf317f90c8bd2bb5294
Anna Anks Nowak <anna.nowak@netguru.pl>
2016-03-20 19:56:40 +0100

    create! instead of create [#88]

--- app/models/facebook_account.rb
+++ app/models/facebook_account.rb
@@ -7,7 +7,7 @@ class FacebookAccount < ActiveRecord::Base
   def self.find_or_create_for_facebook(auth)
     user = find_by(uid: auth['uid'], provider: auth['provider'])
     if user.blank?
-      person = Person.create(first_name: auth['info']['first_name'], last_name: auth['info']['last_name'])
+      person = Person.create!(first_name: auth['info']['first_name'], last_name: auth['info']['last_name'])
       user = create(uid: auth['uid'], provider: auth['provider'], person: person)
     end
   end
